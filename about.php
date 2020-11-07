65ee6191e1124d2866a3982535527f65f40010b1
Myadah <memaaaaaa8@gmail.com>
2020-11-07 19:36:48 +0300

    kkk

---
+++ about.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> About | Stack Code</title>
+    
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="responsive.css">
+    
+    <link href="css/owl.carousel.css/owl.carousel.css" rel="stylesheet">
+    <link href="css/owl.carousel.css/owl.theme.css" rel="stylesheet">
+    
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  
+  <body>
+
+   
+  <section class="wpm_title_ber">
+  
+  	<div class="wpm_opacity_bg">
+   
+		<div class="container">
+        
+        	<div class="row">
+            
+            	<div class="col-sm-12">
+                
+                	<h2>About Us</h2>
+                    
+                    <div class="ber_link text-center">
+                    
+                    	<h5 class="sub_title">WE HAVE 25 YEARS EXERIENCE IN Stack Code</h5>
+                    
+                        <p> <i class="fa fa-angle-double-right"></i> <a href="index.html">Home</a> / <a href="about.html">About Us</a>  <i class="fa fa-angle-double-left"></i> </p>
+                    
+                   </div>
+                
+                </div>
+            
+            </div>
+        
+        </div> 
+        
+      </div>    
+   
+   </section>
+   
+   <section>
+   <img src="02.png" width="100px" hieght="10px"/>
+   </section>
+   
+   <section class="wpm_team_area">
+   
+   		<div class="container">
+        
+        	<div class="row">
+            
+            	<h2>Information <span class="wpm_color_word">Stack Code</span></h2>
+                
+                <div class="wpm_border"> <i class="fa fa-smile-o"></i> </div>
+                
+                <div class="col-sm-4">
+                
+                    <div class="wpm_col_text">
+                        <div class="imageOuter" style="margin:0">
+                           
+                            <a class="image"  href="#">
+                                <span class="rollover" ></span>
+                                <img class="imgborder" alt="" src="01.png">			
+                            </a> 
+                               
+                        </div>
+                        
+                            <h4>QA</h4>                        
+                    </div>
+                
+                </div>
+                
+                <div class="col-sm-4">
+                
+                <div class="wpm_col_text">
+                    <div class="imageOuter" style="margin:0">
+                       
+                        <a class="image"  href="#">
+                            <span class="rollover" ></span>
+                            <img class="imgborder" alt="" src="04.png">			
+                        </a> 
+                           
+                    </div>
+                    
+                        <h4>Security</h4>                        
+                </div>
+            
+            </div>
+
+                <div class="col-sm-4">
+                
+                    <div class="wpm_col_text">
+                        <div class="imageOuter" style="margin:0">
+                           
+                            <a class="image"  href="#">
+                                <span class="rollover" ></span>
+                                <img class="imgborder" alt="" src="03.png">			
+                            </a> 
+                               
+                        </div>
+                        
+                            <h4>WEB</h4>                        
+                    </div>
+                
+                </div>
+            
+            </div>
+        
+        </div>
+   
+   </section>
+   <br/><br/>
+   
+   <footer class="wpm_footer">
+   
+   		<div class="container">
+        
+        	<div class="row">
+            
+            	<div class="col-sm-4">
+                	
+                    <h2>ABOUT STACK CODE</h2>
+                    
+                    <p>The new Stack for integrated software solutions, analysis and design</p>		
+                    
+                    <ul class="wpm_list">
+                    
+                    	<li> <i class="fa fa-location-arrow fa-fw"></i> Riyadh, Saudi Arabia</li>
+                        
+                        <li> <i class="fa fa-phone fa-fw"></i> +8801354702974</li>
+                        
+                        <li> <i class="fa fa-envelope fa-fw"></i><a href=""> info@Stack.com</li>
+                    
+                    </ul>
+                    
+                    <h2 class="stay_text">STAY CONNECTED</h2>
+                    
+                    	<a class="social_icon" href="#"><i class="fa fa-facebook"></i></a>
+                        <a class="social_icon" href="#"><i class="fa fa-google-plus"></i></a>
+                        <a class="social_icon" href="#"><i class="fa fa-twitter"></i></a>
+                        <a class="social_icon" href="#"><i class="fa fa-youtube"></i></a>
+                        
+                </div>
+                
+                <div class="col-sm-4">
+                
+                	<h2>FEATURED ARTICLES</h2>
+                    
+                    <div class="footer_some_title">
+                    
+                    	<img src="01.png" alt="">
+                        
+                        <h4><a href="#">QA</a></h4>
+                        
+                           <a href="#" class="">July 4, 2014 03:57 pm</a>
+                    
+                    </div>
+                    <br/>
+
+                    <div class="footer_some_title">
+                    
+                    	<img src="03.png" alt="">
+                        
+                        <h4><a href="#">WEB</a></h4>
+                        
+                           <a href="#" class="">July 4, 2014 03:57 pm</a>
+                    
+                    </div>
+                    <br/>
+
+                    <div class="footer_some_title">
+                    
+                    	<img src="04.png" alt="">
+                        
+                        <h4><a href="#">Security</a></h4>
+                        
+                           <a href="#" class="">July 4, 2014 03:57 pm</a>
+                    
+                    </div>
+                    <br/>
+                    <div class="footer_some_title">
+                    
+                    	<img src="05.png" alt="">
+                        
+                        <h4><a href="#">.NET</a></h4>
+                        
+                           <a href="#" class="">July 4, 2014 03:57 pm</a>
+                    
+                    </div>
+                
+                </div>
+                
+            </div>
+        
+        </div>
+   
+   </footer>   
+   
+   </body>
+   </html>
